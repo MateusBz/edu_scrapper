e3eae18ab0caaf361f3131cf4ef7e1ef298da541
mateusz <mateusz.bzdzion@gmail.com>
2021-03-03 21:58:28 +0100

    create find_next_links file

---
+++ find_next_links.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+from requests import get
+
+
+def find_next_url(url):
+    start_link = url
+    page = get(start_link)
+    bs = BeautifulSoup(page.content, 'html.parser')
+    blog_pagination = bs.find('nav', class_='blog-pagination clear-fix numeric')
+    page_links = blog_pagination.find_all('a')
+    url_links = [link.get('href') for link in page_links]
+    return url_links
